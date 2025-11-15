dead08ddf78ae659a2843c1a042bdc89a899a461
Haseeb Muneer <haseebmuneer333@gmail.com>
2025-11-15 19:47:49 +0500

    cookie error handle

--- Controller/Auth.js
+++ Controller/Auth.js
@@ -29,7 +29,7 @@ export const register = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      samesite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -65,7 +65,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      samesite: process.env.NODE_ENV === "production" ? "none" : "static",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return res.json({ success: true });
@@ -78,7 +78,7 @@ export const logout = async (req, res) => {
     res.clearCookie("token", {
       httpOnly: false,
       secure: process.env.NODE_ENV === "production",
-      samesite: process.env.NODE_ENV === "production" ? "none" : "static",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
     });
     return res.json({ success: true });
   } catch (error) {
