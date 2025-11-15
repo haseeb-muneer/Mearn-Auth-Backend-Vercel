6c9fb5258b4a6cb72ca956d3179a46555488cd1c
Haseeb Muneer <haseebmuneer333@gmail.com>
2025-11-15 14:43:54 +0500

    adding frontend url

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = "4000" || process.env.PORT;
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-const allowedorigin = ["http://localhost:5173"];
+const allowedorigin = ["https://mearn-auth-vercel-frotend.vercel.app/"];
 app.use(cors({ origin: allowedorigin, credentials: true }));
 connectDB();
 app.use("/api/auth", AuthRouter);
